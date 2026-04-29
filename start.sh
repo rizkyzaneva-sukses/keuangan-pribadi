0c181d3926ad9737f12cca969cb5c820b2622746
rizkyzaneva-sukses <rizkyzaneva@gmail.com>
2026-04-29 14:38:18 +0700

    fix: remove redundant sed command in start.sh causing Permission Denied during container startup

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -e
 
-# Pastikan provider di schema adalah postgresql (sudah diubah saat build, tapi untuk berjaga-jaga)
-sed -i 's/provider = "sqlite"/provider = "postgresql"/g' prisma/schema.prisma
-
+# Pastikan provider di schema adalah postgresql (sudah diubah saat build)
 # Gunakan db push karena migrate deploy butuh file migrasi spesifik Postgres
 npx prisma db push --accept-data-loss
 
